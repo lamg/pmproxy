3b626eeccc3e116e6c6a4bf045ede25cee91bfb9
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2017-10-28 19:11:50 -0400

    TestUserInfo deleted since it depends on environment

--- user_info_test.go
+++ user_info_test.go
@@ -1,48 +1,10 @@
 package pmproxy
 
 import (
-	. "github.com/lamg/ldaputil"
-	"github.com/stretchr/testify/require"
-	"os"
 	"testing"
-)
 
-var (
-	adAddr   = os.Getenv("AD")
-	adSuff   = os.Getenv("AD_SUFF")
-	adBDN    = os.Getenv("AD_BDN")
-	adAdmG   = os.Getenv("AD_ADMG")
-	adQGPref = os.Getenv("AD_QGPREF")
-	uprUser  = os.Getenv("UPR_USER")
-	uprPass  = os.Getenv("UPR_PASS")
+	"github.com/stretchr/testify/require"
 )
-
-func TestUserInfo(t *testing.T) {
-	var ok bool
-	va := []string{adAddr, adSuff, adBDN, adAdmG, adQGPref,
-		uprUser, uprPass}
-	var i int
-	ok, i = true, 0
-	for ok && i != len(va) {
-		ok = va[i] != ""
-		if ok {
-			i = i + 1
-		}
-	}
-	require.True(t, ok, "Env var va[%d] = \"\"", i)
-	udb := NewLDB(adAddr, adSuff, adBDN, adAdmG, adQGPref)
-	u, e := udb.UserInfo(uprUser, uprPass, uprUser)
-	if e != nil && e.Code == ErrorNetwork {
-		t.Log("No network connection")
-	} else {
-		require.True(t, e == nil && len(u.UserName) > 0 &&
-			len(u.Name) > 0 && len(u.QuotaGroup) > 0,
-			"qg: %s", u.QuotaGroup)
-		t.Log(u.QuotaGroup)
-		t.Log(u.IsAdmin)
-		// TODO use actual groups in AD
-	}
-}
 
 func TestElementOf(t *testing.T) {
 	a, b, c := []string{"a", "b", "c"}, []string{"c"}, []string{"C"}
