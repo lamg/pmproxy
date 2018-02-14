ab61f4d72c987e16e1f7f5b6d1ec784e97150c51
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2018-02-14 11:35:59 -0500

    Trying previous task to work

--- local_handler.go
+++ local_handler.go
@@ -73,7 +73,7 @@ func NewLocalHn(qa *QAdm, sp string) (p *LocalHn) {
 func (p *LocalHn) proxyA(w h.ResponseWriter, r *h.Request) {
 	var pth string
 	if r.URL.Path == "/login" {
-		pth = ""
+		pth = "index.html"
 	} else {
 		pth = r.URL.Path[1:]
 	}
